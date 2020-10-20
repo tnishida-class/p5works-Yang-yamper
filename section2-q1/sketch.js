82762ebd7aedbdcdf254b8daca5afaa20430c89f
Yang-yamper <1930502h@stu.kobe-u.ac.jp>
2020-10-20 21:10:19 +0900

    Change section1-1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,16 @@
 // 小手調べ
-function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+function setup() {
+  createCanvas(100,100);
+  stroke(255, 0, 0);
+  for(let i = 0; i < 5; i++){
+
+  let x = 10 *( 10 - i);
+  ellipse(50, 50, x, x);
   }
+  for(let i = 5; i < 10; i++){
+  stroke(0, 0, 255);
+  let x = 10 *( 10 - i);
+  ellipse(50, 50, x, x);
+  }
+
 }
