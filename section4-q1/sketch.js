88390826d2d4bb4b7fb3b7cff2cd44aed28034dd
Yang-yamper <1930502h@stu.kobe-u.ac.jp>
2020-11-21 19:17:30 +0900

    Change 4-q1
    countを使用して作成することができました。

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,23 +1,23 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
-let size;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
-  size = 10;
+  size = 0
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  size += 59
-  if(size > 100){
-    size -= 60
+if(count < 50){
+    size += 3
   }
-
-
+else{
+    size -= 3
+}
   ellipse(width / 2, height / 2, size);
+
 }
